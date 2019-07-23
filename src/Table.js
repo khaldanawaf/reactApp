8b0887fb757604d1d9258b9f387a25371b4e3690
Khalda <53058786+khaldanawaf@users.noreply.github.com>
2019-07-23 09:05:47 +0300

    Add files via upload

--- src/Table.js
+++ src/Table.js
@@ -1,40 +1,34 @@
-import React from 'react';
+import React from "react";
 import {
   Table,
   TableBody,
   TableHeader,
   TableHeaderColumn,
   TableRow,
-  TableRowColumn,
-} from 'material-ui/Table';
+  TableRowColumn
+} from "material-ui/Table";
 
-const row = (x, i, header) => (
-   <TableRow key={'tr=${i}'}>
-        {header.map((y, k) => (
-            <TableRowColumn key={'trc=${k}'}>
-            {x[y.prop]}
-            </TableRowColumn>
-          ))
-        }
-      </TableRow>
-);
+const row = (x, i, header) =>
+  <TableRow key={`tr-${i}`}>
+    {header.map((y, k) =>
+      <TableRowColumn key={`trc-${k}`}>
+        {x[y.prop]}
+      </TableRowColumn>
+    )}
+  </TableRow>;
 
 export default ({ data, header }) =>
   <Table>
     <TableHeader>
       <TableRow>
-      {
-        header.map((x, i) => )
-
-        <TableHeaderColumn key={'thc=${i}'}>
-        {x.name}</TableHeaderColumn>)
-      }
-
+        {header.map((x, i) =>
+          <TableHeaderColumn key={`thc-${i}`}>
+            {x.name}
+          </TableHeaderColumn>
+        )}
       </TableRow>
     </TableHeader>
     <TableBody>
-    {data.map((x, i) => row(x, i, header))}
-
+      {data.map((x, i) => row(x, i, header))}
     </TableBody>
-  </Table>
-);
+  </Table>;
