078e2d5422b486d4f82c1f284318bebb70a6c369
khaldanawaf <khaldanawaf@gmail.com>
2019-07-23 05:54:55 +0300

    frist commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Table from "./Table"
+
+class App extends Component {
+  state = {
+    data: []
+  };
 
 function App() {
   return (
@@ -19,6 +25,38 @@ function App() {
           Learn React
         </a>
       </header>
+      <Form
+      onSubmit={submission =>
+        this.setState({
+        data: [...this.state.data, submission]
+
+      })}
+      />
+      <Table
+      data={this.state.data}
+      header={[
+        {
+      name:  "Frist name",
+      prop: "fristName"
+    },
+      {
+        name: "Last name",
+        prop:"LastName"
+      },
+      {
+        name:  "University",
+        prop:"university"
+        },
+        {
+          name: "Major",
+          prop:"major"
+         },
+         {
+          name:  "CV",
+          prop:"cv"
+          },
+          ]}
+          />
     </div>
   );
 }
